66248059892eed736b8c1730ee8dfb01acba74e4
Ingrid Pauchon <ingridpauchon@gmail.com>
2020-09-23 14:14:20 +0200

    Made the Yelp MVP work, doesn't look perfect, but it works, moving on to the next challenge, will make it beautiful later

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.create( name: "Katla", address:'Oslo', phone_number: '90909090', category: 'japanese')
+Restaurant.create( name: "Eataly", address:'Rome', phone_number: '90909091', category: 'italian')
+Restaurant.create( name: "Wafflez", address:'Brussels', phone_number: '90909092', category: 'belgian')
+Restaurant.create( name: "Pekino", address:'Pekin', phone_number: '90909093', category: 'chinese')
+Restaurant.create( name: "Miam", address:'Paris', phone_number: '90909094', category: 'french')
